1f1e34dd79701c6f593dbe6af61ced511e09a2ff
Colin Gourlay <Gourlay.Colin@abc.net.au>
2019-03-07 10:54:21 +1000

    Add return types. Fix build parameters.

--- src/index.ts
+++ src/index.ts
@@ -10,41 +10,7 @@ const MACHINE_ID_BIT_MASK = new BigInteger('31');
 const SEQUENCE_ID_BIT_MASK = new BigInteger('4095');
 const WORKER_ID_BIT_MASK = new BigInteger('1023');
 
-const getCreationTime = (id: string) =>
-  new BigInteger(id)
-    .shiftRight(CREATION_TIME_SHIFT_PLACES)
-    .add(CREATION_TIME_EPOCH)
-    .toString();
-
-const getDataCenterId = (id: string) =>
-  new BigInteger(id)
-    .shiftRight(DATA_CENTER_ID_SHIFT_PLACES)
-    .and(DATA_CENTER_ID_BIT_MASK)
-    .toString();
-
-const getMachineId = (id: string) =>
-  new BigInteger(id)
-    .shiftRight(MACHINE_ID_SHIFT_PLACES)
-    .and(MACHINE_ID_BIT_MASK)
-    .toString();
-
-const getSequenceId = (id: string) => new BigInteger(id).and(SEQUENCE_ID_BIT_MASK).toString();
-
-const getWorkerId = (id: string) =>
-  new BigInteger(id)
-    .shiftRight(WORKER_ID_SHIFT_PLACES)
-    .and(WORKER_ID_BIT_MASK)
-    .toString();
-
-const getComponents = (id: string) => ({
-  creationTime: getCreationTime(id),
-  dataCenterId: getDataCenterId(id),
-  machineId: getMachineId(id),
-  sequenceId: getSequenceId(id),
-  workerId: getWorkerId(id)
-});
-
-const generateId = (creationTime: string, workerId: string, sequenceId: string) =>
+const generateId = (creationTime: string, workerId: string, sequenceId: string): string =>
   new BigInteger(creationTime)
     .subtract(CREATION_TIME_EPOCH)
     .shiftLeft(CREATION_TIME_SHIFT_PLACES)
@@ -52,13 +18,38 @@ const generateId = (creationTime: string, workerId: string, sequenceId: string) 
     .add(new BigInteger(sequenceId))
     .toString();
 
-export {
-  CREATION_TIME_EPOCH,
-  generateId,
-  getComponents,
-  getCreationTime,
-  getDataCenterId,
-  getMachineId,
-  getSequenceId,
-  getWorkerId
-};
+const getCreationTime = (id: string): string =>
+  new BigInteger(id)
+    .shiftRight(CREATION_TIME_SHIFT_PLACES)
+    .add(CREATION_TIME_EPOCH)
+    .toString();
+
+const getDataCenterId = (id: string): string =>
+  new BigInteger(id)
+    .shiftRight(DATA_CENTER_ID_SHIFT_PLACES)
+    .and(DATA_CENTER_ID_BIT_MASK)
+    .toString();
+
+const getMachineId = (id: string): string =>
+  new BigInteger(id)
+    .shiftRight(MACHINE_ID_SHIFT_PLACES)
+    .and(MACHINE_ID_BIT_MASK)
+    .toString();
+
+const getSequenceId = (id: string): string => new BigInteger(id).and(SEQUENCE_ID_BIT_MASK).toString();
+
+const getWorkerId = (id: string) =>
+  new BigInteger(id)
+    .shiftRight(WORKER_ID_SHIFT_PLACES)
+    .and(WORKER_ID_BIT_MASK)
+    .toString();
+
+const getComponents = (id: string): object => ({
+  creationTime: getCreationTime(id),
+  dataCenterId: getDataCenterId(id),
+  machineId: getMachineId(id),
+  sequenceId: getSequenceId(id),
+  workerId: getWorkerId(id)
+});
+
+export { generateId, getComponents, getCreationTime, getDataCenterId, getMachineId, getSequenceId, getWorkerId };
